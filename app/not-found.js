159a7baa74a67e350c9de7587a0151a6b6e6fbfc
Yasin Ertune <yasinertune1907@gmail.com>
2025-03-24 16:17:50 +0300

    add: meals grid  page

--- app/not-found.js
+++ app/not-found.js
@@ -2,9 +2,9 @@
 
 export default function NotFound() {
     return <main className="not-found">
-        <h1>Not found</h1>
+        <h1>Meal Not found</h1>
         <p>
-            Unfortunately, we could not find the requested or resource.
+            Unfortunately, we could not find the requested or meal data.
         </p>
     </main>
 }
